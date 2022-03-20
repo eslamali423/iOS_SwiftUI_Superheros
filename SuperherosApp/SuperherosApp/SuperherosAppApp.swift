1ef90bbfb83f7def5f49c5819dd4310258c88ea2
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-20 17:30:11 +0200

    Initial Commit

---
+++ SuperherosApp/SuperherosApp/SuperherosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperherosAppApp.swift
+//  SuperherosApp
+//
+//  Created by Eslam Ali  on 20/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperherosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
